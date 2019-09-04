654fc7b185afcf5e3f1610b45b36ddcf611ad488
AnfalHussain <anfalae95@gmail.com>
2019-09-04 14:57:03 +0300

    Anfal

--- flights/serializers.py
+++ flights/serializers.py
@@ -22,24 +22,49 @@ class BookingDetailsSerializer(serializers.ModelSerializer):
 		fields = ['flight', 'date', 'passengers', 'id']
 
 
+
+class UpdateBooking2Serializer(serializers.ModelSerializer):
+	class Meta:
+		model = Booking
+		fields = ['passengers']
+
+
 class UpdateBookingSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Booking
 		fields = ['date', 'passengers']
 
 
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-    class Meta:
-        model = User
-        fields = ['username', 'password', 'first_name', 'last_name']
+	# def perform_update(self, serializer):
+	#     if self.request.user.is_staff:
+	#         myDate = self.kwargs['date']
+	#         mypassengers = self.kwargs['passengers']
 
-    def create(self, validated_data):
-        username = validated_data['username']
-        password = validated_data['password']
-        first_name = validated_data['first_name']
-        last_name = validated_data['last_name']
-        new_user = User(username=username, first_name=first_name, last_name=last_name)
-        new_user.set_password(password)
-        new_user.save()
-        return validated_data
+
+
+	# def update(self, request, *args, **kwargs):
+	#     partial = kwargs.pop('partial', False)
+	#     instance = self.get_object()
+	#     serializer = self.get_serializer(instance, data=request.data, partial=partial)
+	#     serializer.is_valid(raise_exception=True)
+	#     self.perform_update(serializer)
+	#     return Response(serializer.data)
+	#     serializer.save()
+
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+	password = serializers.CharField(write_only=True)
+	class Meta:
+		model = User
+		fields = ['username', 'password', 'first_name', 'last_name']
+
+	def create(self, validated_data):
+		username = validated_data['username']
+		password = validated_data['password']
+		first_name = validated_data['first_name']
+		last_name = validated_data['last_name']
+		new_user = User(username=username, first_name=first_name, last_name=last_name)
+		new_user.set_password(password)
+		new_user.save()
+		return validated_data
--- flights/views.py
+++ flights/views.py
@@ -2,7 +2,7 @@ from rest_framework.generics import ListAPIView, RetrieveAPIView, RetrieveUpdate
 from datetime import datetime
 
 from .models import Flight, Booking
-from .serializers import FlightSerializer, BookingSerializer, BookingDetailsSerializer, UpdateBookingSerializer, RegisterSerializer
+from .serializers import FlightSerializer, BookingSerializer, BookingDetailsSerializer, UpdateBookingSerializer, RegisterSerializer, UpdateBooking2Serializer
 
 
 class FlightsList(ListAPIView):
@@ -11,8 +11,14 @@ class FlightsList(ListAPIView):
 
 
 class BookingsList(ListAPIView):
-	queryset = Booking.objects.filter(date__gte=datetime.today())
 	serializer_class = BookingSerializer
+
+	def get_queryset(self):
+		query = Booking.objects.filter(date__gte=datetime.today(),
+			user= self.request.user)
+		return query
+
+
 
 
 class BookingDetails(RetrieveAPIView):
@@ -24,9 +30,76 @@ class BookingDetails(RetrieveAPIView):
 
 class UpdateBooking(RetrieveUpdateAPIView):
 	queryset = Booking.objects.all()
-	serializer_class = UpdateBookingSerializer
 	lookup_field = 'id'
 	lookup_url_kwarg = 'booking_id'
+
+	def get_serializer_class(self):
+		if self.request.user.is_staff:
+			return UpdateBookingSerializer
+		else:
+			return UpdateBooking2Serializer
+
+		
+
+
+	# def get_serializer(self, *args, **kwargs):
+	# 	if self.request.user.is_staff:
+	# 		serializer_class = UpdateBookingSerializer
+	# 	else:
+	# 		serializer_class = UpdateBooking2Serializer
+			
+	# 	serializer_class = self.get_serializer_class()
+	# 	kwargs['context'] = self.get_serializer_context()
+	# 	return serializer_class(*args, **kwargs)
+
+	# def get_serializer_class(self):
+
+
+	# 	assert self.serializer_class is not None, (
+	# 		if self.request.user.is_staff:
+	# 			serializer_class = UpdateBookingSerializer
+	# 		else:
+	# 			serializer_class = UpdateBooking2Serializer
+
+ #            % self.__class__.__name__
+ #        )
+
+	# 	return self.serializer_class	
+
+
+	# def perform_update(self, serializer):
+	# 	if self.request.user.is_staff:
+	# 		serializer_class = UpdateBookingSerializer
+	# 	else:
+	# 		serializer_class = UpdateBooking2Serializer
+
+	# 	serializer.save()
+
+	# def define_serializer_class(self, UpdateBookingSerializer, UpdateBooking2Serializer ):
+	# 	if self.request.user.is_staff:
+	# 		serializer_class = UpdateBookingSerializer
+	# 	else:
+	# 		serializer_class = UpdateBooking2Serializer
+
+	# 	return serializer_class 	
+
+	
+
+	# if self.request.user.is_staff:
+	# 	serializer_class = UpdateBookingSerializer
+
+	# serializer_class = UpdateBooking2Serializer	
+
+
+	# def partial_update(self, request, *args, **kwargs):
+	# 	if self.request.user.is_staff:
+	# 		serializer_class = UpdateBookingSerializer
+
+	# 	serializer_class = UpdateBooking2Serializer	
+
+	# 	return self.update(request, *args, **kwargs)
+
+
 
 
 class CancelBooking(DestroyAPIView):
